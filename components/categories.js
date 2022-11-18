30b4ddc3a56ba287d1deb7fba0e799216e17f17f
Athul <athulakumar10@gmail.com>
2022-11-18 11:37:14 +0530

    hoempage ui..5

---
+++ components/categories.js
@@ -0,0 +1,167 @@
+function Categ(){
+    return <section className="text-gray-600 body-font ">
+          <div className="container  px-32 py-5 mx-auto flex flex-wrap">
+    <div className="flex flex-wrap -m-1">
+      <div className="p-2 lg:w-full md:w-full">
+        <div className="py-12">
+            <h1 className="font-bold text-3xl text-[#FFFFFF]">Explore by Category</h1>
+        </div>
+        <div className=" grid grid-cols-3 gap-14">
+            {/* card1 start */}
+         <div className="flex justify-center flex-col w-96 h-72 bg-transparent border-2 shadow-md  border-transparent shadow-[#2EADC5] hover:border-[#2EADC5] rounded-[30px]  ">
+        <div className="grid grid-row-2 gap-3 flex">
+            <div className="bg-[#2EADC5]  justify-center flex-row h-58 rounded-[28px]">
+                <img src="art.svg" className="flex justify-center  h-52 "/>
+          {/* <h2 className="flex  justify-center text-2xl text-[#f8fafc] font-semibold">Artwork </h2> */}
+          </div>
+          <div className="bg-transparent justify-center flex-row h-16">
+             <p className=" flex  justify-center text-[#FFFFFF] text-2xl font-bold  py-2 "> ART</p>
+             </div>
+             </div>
+             <a></a>
+         </div>
+         {/* card end */}
+
+
+         {/* card1 start */}
+         <div className="flex justify-center flex-col w-96 h-72 bg-transparent border-2 shadow-md  border-transparent shadow-[#2EADC5] hover:border-[#2EADC5] rounded-[30px]  ">
+        <div className="grid grid-row-2 gap-3 flex">
+            <div className="bg-[#2EADC5]  justify-center flex-row h-58 rounded-[28px]">
+                <img src="music.svg" className="flex justify-center  h-52 "/>
+          {/* <h2 className="flex  justify-center text-2xl text-[#f8fafc] font-semibold">Artwork </h2> */}
+          </div>
+          <div className="bg-transparent justify-center flex-row h-16">
+             <p className=" flex  justify-center text-[#FFFFFF] text-2xl font-bold  py-2 ">MUSIC</p>
+             </div>
+             </div>
+             <a></a>
+         </div>
+         {/* card end */}
+
+
+         {/* card1 start */}
+         <div className="flex justify-center flex-col w-96 h-72 bg-transparent border-2 shadow-md  border-transparent shadow-[#2EADC5] hover:border-[#2EADC5] rounded-[30px]  ">
+        <div className="grid grid-row-2 gap-3 flex">
+            <div className="bg-[#2EADC5]  justify-center flex-row h-58 rounded-[28px]">
+                <img src="tradingcard.svg" className="flex justify-center  h-52 "/>
+          {/* <h2 className="flex  justify-center text-2xl text-[#f8fafc] font-semibold">Artwork </h2> */}
+          </div>
+          <div className="bg-transparent justify-center flex-row h-16">
+             <p className=" flex  justify-center text-[#FFFFFF] text-2xl font-bold  py-2 ">TRADING CARDS</p>
+             </div>
+             </div>
+             <a></a>
+         </div>
+         {/* card end */}
+
+
+         {/* card1 start */}
+         <div className="flex justify-center flex-col w-96 h-72 bg-transparent border-2 shadow-md  border-transparent shadow-[#2EADC5] hover:border-[#2EADC5] rounded-[30px]  ">
+        <div className="grid grid-row-2 gap-3 flex">
+            <div className="bg-[#2EADC5]  justify-center flex-row h-58 rounded-[28px]">
+                <img src="collectibles.svg" className="flex justify-center  h-52 "/>
+          {/* <h2 className="flex  justify-center text-2xl text-[#f8fafc] font-semibold">Artwork </h2> */}
+          </div>
+          <div className="bg-transparent justify-center flex-row h-16">
+             <p className=" flex  justify-center text-[#FFFFFF] text-2xl font-bold  py-2 ">COLLECTIBLES</p>
+             </div>
+             </div>
+             <a></a>
+         </div>
+         {/* card end */}
+
+         {/* card1 start */}
+         <div className="flex justify-center flex-col w-96 h-72 bg-transparent border-2 shadow-md  border-transparent shadow-[#2EADC5] hover:border-[#2EADC5] rounded-[30px]  ">
+        <div className="grid grid-row-2 gap-3 flex">
+            <div className="bg-[#2EADC5]  justify-center flex-row h-58 rounded-[28px]">
+                <img src="photography.svg" className="flex justify-center  h-52 "/>
+          {/* <h2 className="flex  justify-center text-2xl text-[#f8fafc] font-semibold">Artwork </h2> */}
+          </div>
+          <div className="bg-transparent justify-center flex-row h-16">
+             <p className=" flex  justify-center text-[#FFFFFF] text-2xl font-bold  py-2 ">PHOTOGRAPHY</p>
+             </div>
+             </div>
+             <a></a>
+         </div>
+         {/* card end */}
+
+         {/* card1 start */}
+         <div className="flex justify-center flex-col w-96 h-72 bg-transparent border-2 shadow-md  border-transparent shadow-[#2EADC5] hover:border-[#2EADC5] rounded-[30px]  ">
+        <div className="grid grid-row-2 gap-3 flex">
+            <div className="bg-[#2EADC5]  justify-center flex-row h-58 rounded-[28px]">
+                <img src="utility.svg" className="flex justify-center  h-52 "/>
+          {/* <h2 className="flex  justify-center text-2xl text-[#f8fafc] font-semibold">Artwork </h2> */}
+          </div>
+          <div className="bg-transparent justify-center flex-row h-16">
+             <p className=" flex  justify-center text-[#FFFFFF] text-2xl font-bold  py-2 ">UTILITY</p>
+             </div>
+             </div>
+             <a></a>
+         </div>
+         {/* card end */}
+
+
+
+         {/* card1 start */}
+         <div className="flex justify-center flex-col w-96 h-72 bg-transparent border-2 shadow-md  border-transparent shadow-[#2EADC5] hover:border-[#2EADC5] rounded-[30px]  ">
+        <div className="grid grid-row-2 gap-3 flex">
+            <div className="bg-[#2EADC5]  justify-center flex-row h-58 rounded-[28px]">
+                <img src="art.svg" className="flex justify-center  h-52 "/>
+          {/* <h2 className="flex  justify-center text-2xl text-[#f8fafc] font-semibold">Artwork </h2> */}
+          </div>
+          <div className="bg-transparent justify-center flex-row h-16">
+             <p className=" flex  justify-center text-[#FFFFFF] text-2xl font-bold  py-2 ">DOMAIN NAMES</p>
+             </div>
+             </div>
+             <a></a>
+         </div>
+         {/* card end */}
+
+         {/* card1 start */}
+         <div className="flex justify-center flex-col w-96 h-72 bg-transparent border-2 shadow-md  border-transparent shadow-[#2EADC5] hover:border-[#2EADC5] rounded-[30px]  ">
+        <div className="grid grid-row-2 gap-3 flex">
+            <div className="bg-[#2EADC5]  justify-center flex-row h-58 rounded-[28px]">
+                <img src="sports.svg" className="flex justify-center  h-52 "/>
+          {/* <h2 className="flex  justify-center text-2xl text-[#f8fafc] font-semibold">Artwork </h2> */}
+          </div>
+          <div className="bg-transparent justify-center flex-row h-16">
+             <p className=" flex  justify-center text-[#FFFFFF] text-2xl font-bold  py-2 ">SPORTS</p>
+             </div>
+             </div>
+             <a></a>
+         </div>
+         {/* card end */}
+
+
+         {/* card1 start */}
+         <div className="flex justify-center flex-col w-96 h-72 bg-transparent border-2 shadow-md  border-transparent shadow-[#2EADC5] hover:border-[#2EADC5] rounded-[30px]  ">
+        <div className="grid grid-row-2 gap-3 flex">
+            <div className="bg-[#2EADC5]  justify-center flex-row h-58 rounded-[28px]">
+                <img src="virtual.svg" className="flex justify-center  h-52 "/>
+          {/* <h2 className="flex  justify-center text-2xl text-[#f8fafc] font-semibold">Artwork </h2> */}
+          </div>
+          <div className="bg-transparent justify-center flex-row h-16">
+             <p className=" flex  justify-center text-[#FFFFFF] text-2xl font-bold  py-2 ">VIRTUAL WORLD</p>
+             </div>
+             </div>
+             <a></a>
+         </div>
+         {/* card end */}
+    
+         
+         </div>
+
+
+          
+          
+         
+ 
+        
+      </div>
+      
+    </div>
+  </div>
+  </section>
+}
+
+export default  Categ;
