2a7c0fb806f8e869904f2fc0e2497326efd28e4f
Athul <athulakumar10@gmail.com>
2022-11-30 00:00:31 +0530

    carousel image

--- components/carousel.js
+++ components/carousel.js
@@ -6,63 +6,63 @@
     return <div className="flex justify-center py-4 bg-gradient-to-tr from-[#2EADC500] to-[#2EADC558]/40">
      
       
-      <h1 className="block"> Hello</h1>  
+      {/* <h1 className="block"> Hello</h1>   */}
      
      
-<div className="relative z-30 scale-75 rounded-lg shadow-md shadow-[#2EADC5]/50 bg-[#152022] max-w-sm md:w-1/2 max-h-1/6 ">
+<div className="relative z-30 scale-75 rounded-t-[32px] shadow-md shadow-[#2EADC5]/50 bg-[#152022] max-w-sm md:w-1/2 max-h-1/6 ">
       <a href="#!">
         <div className= "relative">
          
-        <img className="rounded-t-lg" src="https://mdbootstrap.com/img/new/standard/nature/184.jpg" alt=""/>
+        <img className="object-cover h-72 w-full rounded-t-[32px]" src="car1.svg" alt=""/>
         <button class="absolute top-2 right-2 bg-[#152022] text-white p-2 rounded-[12px] hover:bg-blue-800">
             <img className="flex justify-center w-10 h-10" src="heart.svg" alt=""fav/>
         </button>
         </div>
       </a>
       <div className="p-2 relative">
-        <h5 className="text-white text-2xl font-extrabold mb-2">Fancy Boy</h5>
+        <h5 className="text-white text-2xl font-extrabold mb-2">Owl bliss</h5>
         <button class="absolute top-3 right-2 bg-[#152022] text-white p-2 rounded-[12px] border-2 border-[#2EADC5]/50 ">0.25   ETH</button>
         <p className="text-white text-base font-bold mb-4">
-         by Biker
+         by Renyl
         </p>
        
       </div>
     </div>
     
        
-    <div className="relative z-40 rounded-lg shadow-md shadow-[#2EADC5]/50 bg-[#152022] max-w-sm md:w-1/2 max-h-1/6">
+    <div className="relative z-40 rounded-t-[32px] shadow-md shadow-[#2EADC5]/50 bg-[#152022] max-w-sm md:w-1/2 max-h-1/6">
       <a href="#!">
         <div className= "relative">
-        <img className="rounded-t-lg" src="https://mdbootstrap.com/img/new/standard/nature/184.jpg" alt=""/>
+        <img className="object-cover h-72 w-full rounded-t-[32px]" src="car2.svg" alt=""/>
         <button class="absolute top-2 right-2 bg-[#152022] text-white p-2 rounded-[12px] hover:bg-blue-800">
             <img className="flex justify-center w-10 h-10" src="heart.svg" alt=""fav/>
         </button>
         </div>
       </a>
       <div className="p-2 relative">
-        <h5 className="text-white text-2xl font-extrabold mb-2">Fancy Boy</h5>
+        <h5 className="text-white text-2xl font-extrabold mb-2">Green dilemma</h5>
         <button class="absolute top-3 right-2 bg-[#152022] text-white p-2 rounded-[12px] border-2 border-[#2EADC5]/50 ">0.25   ETH</button>
         <p className="text-white text-base font-bold mb-4">
-         by Biker
+         by Narzo
         </p>
        
       </div>
     </div>
 
-    <div className="relative z-30 scale-75 rounded-lg shadow-md shadow-[#2EADC5]/50 bg-[#152022] max-w-sm md:w-1/2 max-h-1/6">
+    <div className="relative z-30 scale-75 rounded-t-[32px] shadow-md shadow-[#2EADC5]/50 bg-[#152022] max-w-sm md:w-1/2 max-h-1/6">
       <a href="#!">
-        <div className= "relative">
-        <img className="rounded-t-lg" src="https://mdbootstrap.com/img/new/standard/nature/184.jpg" alt=""/>
+        <div className= "p-2 relative">
+        <img className="object-cover h-72 w-full rounded-t-[32px]" src="car3.svg" alt=""/>
         <button class="absolute top-2 right-2 bg-[#152022] text-white p-2 rounded-[12px] hover:bg-blue-800">
             <img className="flex justify-center w-10 h-10" src="heart.svg" alt=""fav/>
         </button>
         </div>
       </a>
       <div className="p-2 relative">
-        <h5 className="text-white text-2xl font-extrabold mb-2">Fancy Boy</h5>
+        <h5 className="text-white text-2xl font-extrabold mb-2">Dark Man </h5>
         <button class="absolute top-3 right-2 bg-[#152022] text-white p-2 rounded-[12px] border-2 border-[#2EADC5]/50 ">0.25   ETH</button>
         <p className="text-white text-base font-bold mb-4">
-         by Biker
+         by Chavelo
         </p>
        
       </div>
--- components/footer.js
+++ components/footer.js
@@ -1,19 +1,21 @@
+import Link from "next/link";
 function Footer(){
     return <footer className="text-gray-600 body-font pt-4 ">
     
     <div className="bg-[#152022] h-12">
       <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
-        <p className="text-gray-500 text-sm text-center sm:text-left text-[#2EADC5] ">© 2022 Trizwit.Inc
-          <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" class="text-gray-600 ml-1" target="_blank"></a>
+        <p className="text-gray-500 text-sm text-center sm:text-left text-cyan-500">© 2022 Trizwit.Inc
+          <Link href="https://twitter.com/knyttneve" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank"></Link>
         </p>
         <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
           
-        <p className="text-gray-500 text-sm text-center sm:text-left text-[#2EADC5] mr-6">Privacy Policy
-          <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" class="text-gray-600 ml-1" target="_blank"></a>
+        <p className="text-gray-500 text-sm text-center sm:text-left text-cyan-500 mr-6">Privacy Policy
+          <Link href="https://twitter.com/knyttneve" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank"></Link>
         </p>
         
-        <p className="text-gray-500 text-sm text-center sm:text-left text-[#2EADC5] ">Terms of Service
-          <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" class="text-gray-600 ml-1" target="_blank"></a>
+        <p className="text-gray-500 text-sm text-center sm:text-left text-cyan-500 ">Terms of Service
+
+          <Link href="https://twitter.com/knyttneve" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank"></Link>
         </p> 
           
         </span>
