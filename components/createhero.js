fafb8e7cf8585c9aef0c3840ab80218f8bb1fb2a
ablevargheseme <ablevargheseme@gmail.com>
2022-12-23 18:24:01 +0530

    web3 integration

--- components/createhero.js
+++ components/createhero.js
@@ -1,19 +1,76 @@
-import { Web3Storage } from "web3.storage";
 import Router from "next/router";
 import Web3Modal from "web3modal";
 import { providers, Contract, utils } from "ethers";
-// import { useEffect, useRef, useState } from "react";
 import { CONTRACT_PARENT_ADDRESS, abiContract } from "../constants";
 
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { useCreateAsset, useLivepeerProvider, Player } from "@livepeer/react";
-import React, { useState , useRef, useEffect} from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 function CreateHero() {
+  const [walletConnected, setWalletConnected] = useState(false);
+  // loading is set to true when we are waiting for a transaction to get mined
+  const [loading, setLoading] = useState(false);
+  // numberOfWhitelisted tracks the number of addresses's whitelisted
+  const web3ModalRef = useRef();
+
+  const getProviderOrSigner = async (needSigner = false) => {
+    // Connect to Metamask
+    // Since we store `web3Modal` as a reference, we need to access the `current` value to get access to the underlying object
+    const provider = await web3ModalRef.current.connect();
+    const web3Provider = new providers.Web3Provider(provider);
+
+    // If user is not connected to the Goerli network, let them know and throw an error
+    const { chainId } = await web3Provider.getNetwork();
+    if (chainId !== 5) {
+      window.alert("Change the network to Goerli");
+      throw new Error("Change network to Goerli");
+    }
+
+    if (needSigner) {
+      const signer = web3Provider.getSigner();
+      return signer;
+    }
+    return web3Provider;
+  };
+
+  /*
+    connectWallet: Connects the MetaMask wallet
+  */
+  const connectWallet = async () => {
+    try {
+      // Get the provider from web3Modal, which in our case is MetaMask
+      // When used for the first time, it prompts the user to connect their wallet
+      await getProviderOrSigner();
+      setWalletConnected(true);
+
+      checkIfAddressInWhitelist();
+      getNumberOfWhitelisted();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  useEffect(() => {
+    // if wallet is not connected, create a new instance of Web3Modal and connect the MetaMask wallet
+    if (!walletConnected) {
+      // Assign the Web3Modal class to the reference object by setting it's `current` value
+      // The `current` value is persisted throughout as long as this page is open
+      web3ModalRef.current = new Web3Modal({
+        network: "goerli",
+        providerOptions: {},
+        disableInjectedProvider: false,
+      });
+      connectWallet();
+    }
+  }, [walletConnected]);
+
+  //web3///////////////////
   const livepeerProvider = useLivepeerProvider();
   const [video, setVideo] = useState(null);
+  // const[lastassetid,setLastassetid]=useState("");
+  var lastassetid = "";
   const {
     mutate: createAsset,
     data: assets,
@@ -29,6 +86,24 @@ function CreateHero() {
     ],
   });
 
+  const uploadVideo = async () => {
+    // Calling the createAsset function from the useCreateAsset hook to upload the video
+    console.log("creating asset");
+    createAsset?.();
+  };
+  // When a user clicks on the upload button
+  // When a user clicks on the upload button
+  const handleSubmit = async () => {
+    // Calling the upload video function
+    await uploadVideo();
+  };
+  useEffect(() => {
+    console.log("status");
+    console.log(status);
+    if (status == "success") {
+      console.log("completed video upload");
+    }
+  }, [status]);
   return (
     <>
       <input
@@ -41,12 +116,10 @@ function CreateHero() {
           }
         }}
       />
-
       <button
         disabled={status === "loading" || !createAsset}
         onClick={() => {
-          createAsset?.();
-          console.log("creating asset");
+          handleSubmit();
         }}
       >
         Create Asset
@@ -61,8 +134,8 @@ function CreateHero() {
           </div>
         </div>
       ))}
-
       {error && <div>{error.message}</div>}
+      status:{status}
       <button></button>
       <h1>Livepeer Provider:{livepeerProvider.getConfig().name}</h1>
     </>
